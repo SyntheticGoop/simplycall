ef6b3a5b1a9bc0f00217fabfa93d5b15598b2876
Synthetic Goop <code@synthetic-goop.com>
2025-02-27 23:59:30 +0800

    feat: Add generic http fetch transports

---
+++ src/transports/http/throughFetch.ts
@@ -0,0 +1,71 @@
+// The MIT License (MIT)
+//
+// Copyright (c) 2025 SyntheticGoop
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files(the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and / or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+import { encode, decode } from "../../codec/formData";
+
+/**
+ * Creates a transport that sends data over
+ * http {@link fetch}.
+ */
+export function makeTransportThroughFetch(config: {
+  url: URL;
+}) {
+  return {
+    async sendThroughTransportFallibly(rpc: {
+      ctx: unknown;
+      id: string;
+      args: unknown[];
+    }) {
+      const maybeFormBody = encode(rpc.args);
+
+      if ("err" in maybeFormBody) {
+        throw Error(`Unable to serialize arguments for [${rpc.id}]`, {
+          cause: maybeFormBody.err,
+        });
+      }
+
+      await fetch(config.url, {
+        headers: {
+          "x-simplycall-id": rpc.id,
+        },
+        method: "POST",
+        body: maybeFormBody.ok,
+      }).then(async (result) => {
+        const argtype = result.headers.get("x-simplycall-argtype");
+        if (argtype !== "b" && argtype !== "j")
+          throw Error(
+            `Unable to parse response type [${argtype}] for [${rpc.id}].`,
+          );
+
+        const formData = await result.formData(); // Allow errors to propagate
+        const response = await decode(formData);
+        if ("err" in response) {
+          throw Error(`unable to decode response for [${rpc.id}]`, {
+            cause: response.err,
+          });
+        }
+
+        return response.ok;
+      });
+    },
+  };
+}
